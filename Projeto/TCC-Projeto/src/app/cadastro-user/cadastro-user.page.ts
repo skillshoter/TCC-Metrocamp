ea67d56af200b7f25431e8e38d7fe35ceb5948a9
edu.siqueroli <edu.siqueroli@gmail.com>
2021-03-26 21:33:14 -0300

    Tela Cadastro de Usuario ++ Post API funcionando

--- Projeto/TCC-Projeto/src/app/cadastro-user/cadastro-user.page.ts
+++ Projeto/TCC-Projeto/src/app/cadastro-user/cadastro-user.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HtmlParser } from '@angular/compiler';
 
 @Component({
   selector: 'app-cadastro-user',
@@ -7,19 +9,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroUserPage implements OnInit {
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
 
   ngOnInit() {
 
   }
 
+  loginUser:any;
+  nameUser:any;
+  telUser:any;
+  emailUser:any;
+  senhaUser:any;
+  confSenhaUser:any;
+
   putUser(event:any)
   {
     console.log("Gravar Usuario");
+    if(this.senhaUser == this.confSenhaUser)
+    {
+      const obj = { 
+        login:this.loginUser,
+        nome:this.nameUser,
+        telefone:this.telUser,
+        email:this.emailUser,
+        senha:this.senhaUser
+       };
+       
+     //  headers.append('Access-Control-Allow-Origin', 'http://localhost:3000');
+       
+      const  requestUrl = 'http://25.3.255.44:8080/users/criaUsuario';//ver com nathan 
+      this.http.post(requestUrl, obj, { headers: { 'Content-Type': 'application/json' } }).subscribe(
+        (val) => {
+            console.log("POST call successful value returned in body", 
+                        val);
+        },
+        response => {
+            console.log("POST call in error", response);
+        },
+        () => {
+            console.log("The POST observable is now completed.");
+        });
+    }
+    else
+    {
+      alert("As duas senha devem ser iguais.");
+    }
   }
 
   voltar(event:any)
   {
     console.log("voltar tela");
+    
   }
 }
