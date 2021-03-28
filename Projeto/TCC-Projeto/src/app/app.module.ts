36de9e400ae9315947ca96d01497284fd467980b
edu.siqueroli <edu.siqueroli@gmail.com>
2021-03-28 20:56:32 -0300

    maravilha

--- Projeto/TCC-Projeto/src/app/app.module.ts
+++ Projeto/TCC-Projeto/src/app/app.module.ts
@@ -2,17 +2,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
+import {TabMenuModule} from 'primeng/tabmenu';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MenuModule} from 'primeng/menu';
+import {MegaMenuItem,MenuItem} from 'primeng/api';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule,TabMenuModule,AccordionModule,BrowserAnimationsModule,MenuModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- Projeto/TCC-Projeto/src/app/principal/principal.page.ts
+++ Projeto/TCC-Projeto/src/app/principal/principal.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MegaMenuItem,MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-principal',
@@ -9,8 +10,18 @@ export class PrincipalPage implements OnInit {
 
   constructor() { }
 
+  
+  items: MenuItem[];
+
   ngOnInit() {
+    
+      this.items = [
+          {label: 'Home', icon: 'pi pi-fw pi-home'},
+          {label: 'Calendar', icon: 'pi pi-fw pi-calendar'},
+          {label: 'Edit', icon: 'pi pi-fw pi-pencil'},
+          {label: 'Documentation', icon: 'pi pi-fw pi-file'},
+          {label: 'Settings', icon: 'pi pi-fw pi-cog'}
+      ];
   }
-   
   
 }
