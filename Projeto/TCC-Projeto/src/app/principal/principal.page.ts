2f540610e07f5e175344724fb03710777223185e
edu.siqueroli <edu.siqueroli@gmail.com>
2021-04-02 14:26:41 -0300

    Parametros + Consulta API GET

--- Projeto/TCC-Projeto/src/app/principal/principal.page.ts
+++ Projeto/TCC-Projeto/src/app/principal/principal.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-principal',
@@ -7,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrincipalPage implements OnInit {
 
-  constructor() { }
+  constructor(public http: HttpClient, private route: Router) { }
 
   ngOnInit() {
   }
-   
   
+  NameEmpresa:any;
+  CnpjEmpresa:any;
+  Endereco:any;
+  EmpresasList:any;
+
+  pesquisar(event:any)
+  {
+      console.log("pesquisarNoBack");
+      console.log("valor do empresa:"+this.NameEmpresa);  
+      console.log("valor do cnpjEmpresa:"+this.CnpjEmpresa);
+      console.log("valor do endereco:"+this.Endereco);
+      this.http.get('http://localhost:3000/getEmpresa?nameEmpresa='+this.NameEmpresa+'&cnpj='+this.CnpjEmpresa+'&endereco='+this.Endereco).subscribe((data)=> {
+        console.log(data);
+        this.EmpresasList = data;
+        if(Object.keys(data).length === 0)
+        {
+          alert("Não existe Empresa com estes parametros");
+        }else
+        {
+          console.log("Mostra Grid");
+          //criar a grid com os parametros do this.EmpresasList
+          alert("Grid montada");//remover depois
+        }
+      },(error)=> {
+        console.log(error);
+        alert("Consulta a API não encontrou o endereço.");
+      });
+  }
+
 }
